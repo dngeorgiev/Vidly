e33dd1ef2eef8f2ab49619cf8e3f3363415b9b89
Dani Nedelchev <dngeorgiev@abv.bg>
2022-05-21 02:35:39 +0300

    Initial commit

---
+++ Vidly/Models/ErrorViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Vidly.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
+
